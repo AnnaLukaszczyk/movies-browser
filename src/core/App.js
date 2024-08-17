288c915c8871e9f7b82b5b521b560e004b47b527
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-17 17:01:27 +0200

    added Tile component to App layout

--- src/core/App.js
+++ src/core/App.js
@@ -1,7 +1,8 @@
 import { Container } from "../common/Container";
+import { Tile } from "../common/Tile";
 
 function App() {
-	return <Container>Movies Browser</Container>;
+	return <Container>Movies Browser<Tile></Tile></Container>;
 }
 
 export default App;
