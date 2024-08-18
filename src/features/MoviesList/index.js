be237a619b293deb50e4f8263906289f06a4041f
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-18 13:25:54 +0200

    added MoviesList component

---
+++ src/features/MoviesList/index.js
@@ -0,0 +1,12 @@
+import { Tile } from "../../common/Tile"
+import { List, ListItem, Content } from "./styled";
+
+export const MoviesList = () => (
+    <Content>
+      <List>
+        <ListItem>
+        <Tile />
+        </ListItem>
+      </List>
+    </Content>
+)
