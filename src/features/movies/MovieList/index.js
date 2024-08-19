e3e555be004850b72f53fa8eaf3ac6c281ad5bf2
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-19 12:43:25 +0200

    changed Content to Section, added SectionTitle

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -1,12 +1,14 @@
+import { Section, SectionTitle } from "../../../common/Section"
 import { TileLarge } from "../../../common/Tile";
-import { List, ListItem, Content } from "./styled";
+import { List, ListItem } from "./styled";
 
 export const MovieList = () => (
-	<Content>
+	<Section>
+		<SectionTitle>Popular movies</SectionTitle>
 		<List>
 			<ListItem>
 				<TileLarge title="Mulan" year="2020" tag="Action" />
 			</ListItem>
 		</List>
-	</Content>
+	</Section>
 );
