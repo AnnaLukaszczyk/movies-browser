7fc53d10207d15abbca190b3802240bca28ed6c7
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-20 20:15:40 +0200

    added @media to List

--- src/features/movies/MovieList/styled.js
+++ src/features/movies/MovieList/styled.js
@@ -9,17 +9,16 @@ export const List = styled.ul`
 	padding: 0;
 	list-style: none;
 
-	@media (max-width: ${({ theme }) => theme.breakpoints.desktopSmall}px) {
+	@media (max-width: ${({ theme }) => theme.breakpoints.tabletLarge}px) {
 		grid-template-columns: repeat(3, minmax(200px, 1fr));
 	}
 
-	@media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+	@media (max-width: ${({ theme }) => theme.breakpoints.tabletSmall}px) {
 		grid-template-columns: repeat(2, minmax(200px, 1fr));
 	}
 
 	@media (max-width: ${({ theme }) => theme.breakpoints.mobileLarge}px) {
 		grid-template-columns: minmax(200px, 1fr);
-
 	}
 `;
 
