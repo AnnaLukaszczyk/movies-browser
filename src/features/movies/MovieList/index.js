4cffd9b14f327d7efb813b653fc36e46ba17c1f7
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-20 20:15:05 +0200

    isLargeScreen changed min-width to 993px. Code formatted.

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -1,12 +1,11 @@
-import { useMediaQuery } from 'react-responsive';
-import { Section, SectionTitle } from "../../../common/Section"
+import { useMediaQuery } from "react-responsive";
+import { Section, SectionTitle } from "../../../common/Section";
 import { TileLarge, TileSmall } from "../../../common/Tile";
 import { List, ListItem, StyledLink } from "./styled";
-import { Main } from '../../../common/Main';
+import { Main } from "../../../common/Main";
 
 export const MovieList = () => {
-
-	const isLargeScreen = useMediaQuery({ query: '(min-width: 992px)' })
+	const isLargeScreen = useMediaQuery({ query: "(min-width: 993px)" });
 
 	return (
 		<Main>
@@ -23,18 +22,19 @@ export const MovieList = () => {
 									year="2020"
 									tag="Action"
 								/>
-							) : <TileSmall
-								ratingValue="8.1"
-								voteAmount="2000"
-								title="Mulan"
-								year="2020"
-								tag="Action"
-							/>}
+							) : (
+								<TileSmall
+									ratingValue="8.1"
+									voteAmount="2000"
+									title="Mulan"
+									year="2020"
+									tag="Action"
+								/>
+							)}
 						</ListItem>
 					</StyledLink>
 				</List>
 			</Section>
-		</Main >
-	)
+		</Main>
+	);
 };
-
