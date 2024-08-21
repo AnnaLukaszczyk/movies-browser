85a3ba980fdf99ce1a5232017e8a653cfa07106b
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-08-21 13:38:25 +0200

    Main margin 56px, media breakpoints added

--- src/common/Main/index.js
+++ src/common/Main/index.js
@@ -2,5 +2,13 @@ import styled from "styled-components";
 
 export const Main = styled.main`
 	max-width: 1368px;
-	margin: auto;
+	margin: 56px auto;
+
+	@media(max-width: ${({ theme }) => theme.breakpoints.desktopSmall}px) { 
+        margin: 40px auto;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileLarge}px) { 
+        margin: 24px auto;
+    }
 `;
