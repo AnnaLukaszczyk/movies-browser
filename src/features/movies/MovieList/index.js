a8888ba750037c4fef8d4a9ec9b63b5343065e06
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-21 16:16:23 +0200

    deleted <Main>

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -2,14 +2,12 @@ import { useMediaQuery } from "react-responsive";
 import { Section, SectionTitle } from "../../../common/Section";
 import { TileLarge, TileSmall } from "../../../common/Tile";
 import { List, ListItem, StyledLink } from "./styled";
-import { Main } from '../../../common/Main';
 import { toMovieDetails } from '../../../core/routes';
 
 export const MovieList = () => {
 	const isLargeScreen = useMediaQuery({ query: "(min-width: 993px)" });
 
 	return (
-		<Main>
 			<Section>
 				<SectionTitle>Popular movies</SectionTitle>
 				<List>
@@ -36,6 +34,5 @@ export const MovieList = () => {
 					</StyledLink>
 				</List>
 			</Section>
-		</Main>
 	);
 };
