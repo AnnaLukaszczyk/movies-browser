74093758e6184ef40c351c27aa12e5812bddb2e4
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-22 12:55:27 +0200

    formatted code

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -2,37 +2,37 @@ import { useMediaQuery } from "react-responsive";
 import { Section, SectionTitle } from "../../../common/Section";
 import { TileLarge, TileSmall } from "../../../common/Tile";
 import { List, ListItem, StyledLink } from "./styled";
-import { toMovieDetails } from '../../../core/routes';
+import { toMovieDetails } from "../../../core/routes";
 
 export const MovieList = () => {
 	const isLargeScreen = useMediaQuery({ query: "(min-width: 993px)" });
 
 	return (
-			<Section>
-				<SectionTitle>Popular movies</SectionTitle>
-				<List>
-					<StyledLink to={toMovieDetails()}>
-						<ListItem>
-							{isLargeScreen ? (
-								<TileLarge
-									ratingValue="8.1"
-									voteAmount="2000"
-									title="Mulan"
-									year="2020"
-									tag="Action"
-								/>
-							) : (
-								<TileSmall
-									ratingValue="8.1"
-									voteAmount="2000"
-									title="Mulan"
-									year="2020"
-									tag="Action"
-								/>
-							)}
-						</ListItem>
-					</StyledLink>
-				</List>
-			</Section>
+		<Section>
+			<SectionTitle>Popular movies</SectionTitle>
+			<List>
+				<StyledLink to={toMovieDetails()}>
+					<ListItem>
+						{isLargeScreen ? (
+							<TileLarge
+								ratingValue="8.1"
+								voteAmount="2000"
+								title="Mulan"
+								year="2020"
+								tag="Action"
+							/>
+						) : (
+							<TileSmall
+								ratingValue="8.1"
+								voteAmount="2000"
+								title="Mulan"
+								year="2020"
+								tag="Action"
+							/>
+						)}
+					</ListItem>
+				</StyledLink>
+			</List>
+		</Section>
 	);
 };
