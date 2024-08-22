8be88955fbd919ce0696c5d5a47d85be55847c3f
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-22 18:16:33 +0200

    added @media for mobileSmall for List

--- src/features/people/PeopleList/styled.js
+++ src/features/people/PeopleList/styled.js
@@ -3,22 +3,27 @@ import styled from "styled-components";
 
 export const List = styled.ul`
 	display: grid;
-	grid-template-columns: repeat(6, minmax(160px, 1fr));
+	grid-template-columns: repeat(6, minmax(150px, 1fr));
 	grid-gap: 24px;
 	margin: 24px auto 0;
 	padding: 0;
 	list-style: none;
 
 	@media (max-width: ${({ theme }) => theme.breakpoints.desktopSmall}px) {
-		grid-template-columns: repeat(5, minmax(160px, 1fr));
+		grid-template-columns: repeat(5, minmax(150px, 1fr));
 	}
 
 	@media (max-width: ${({ theme }) => theme.breakpoints.tabletSmall}px) {
-		grid-template-columns: repeat(4, minmax(160px, 1fr));
+		grid-template-columns: repeat(4, minmax(150px, 1fr));
 	}
 
 	@media (max-width: ${({ theme }) => theme.breakpoints.mobileLarge}px) {
-		grid-template-columns: repeat(3, minmax(160px, 1fr));
+		grid-template-columns: repeat(3, minmax(150px, 1fr));
+	}
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobileSmall}px) {
+		margin: 12px auto 0;
+		grid-template-columns: repeat(2, minmax(136px, 1fr));
 	}
 `;
 
