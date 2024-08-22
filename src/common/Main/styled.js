f01d0df00f91133fd18c6f219d7bc0bb5b53a3e6
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-22 18:07:46 +0200

    changed margin widths in @media

--- src/common/Main/styled.js
+++ src/common/Main/styled.js
@@ -4,11 +4,11 @@ export const StyledMain = styled.main`
 	max-width: 1368px;
 	margin: 56px auto;
 
-	@media(max-width: ${({ theme }) => theme.breakpoints.desktopSmall}px) { 
-        margin: 40px auto;
+	@media(max-width: ${({ theme }) => theme.breakpoints.desktopLarge}px) { 
+        margin: 56px 4%;
     }
 
-    @media(max-width: ${({ theme }) => theme.breakpoints.mobileLarge}px) { 
-        margin: 24px auto;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileSmall}px) { 
+        margin: 24px 16px;
     }
 `;
