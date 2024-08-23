a4ca56dd1af91cdf5dbc40c8923b6d8545cbf500
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-23 14:03:45 +0200

    Added style for different screen size

--- src/common/Rating/styled.js
+++ src/common/Rating/styled.js
@@ -7,11 +7,21 @@ export const RatingWrapper = styled.div`
     gap:12px;
     align-items: center;
 
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileLarge}px) {
+		grid-gap: 4px;
+        }
+
     ${({ $onMoviePhoto }) => $onMoviePhoto && css`
         flex-wrap: wrap;
         width: 150px;
 	    height: 75px;
         gap: 8px;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.tabletSmall}px) {
+		flex-wrap: nowrap;
+        width:auto;
+        height:auto;
+        }
 	`}
 `;
 
@@ -19,9 +29,19 @@ export const StarIcon = styled(StarSVG)`
     width: 24px;
     height: 24px;
 
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileSmall}px) {
+		width: 16px;
+        height:16px;
+        }
+
     ${({ $onMoviePhoto }) => $onMoviePhoto && css`
-        width: 40px;
-        height: 40px;
+       
+
+        @media (min-width: ${({ theme }) => theme.breakpoints.mobileLarge}px) {
+		width: 40px;
+        height:40px;
+        }
+        
     `}
 `;
 
@@ -31,11 +51,19 @@ export const RatingValue = styled.span`
     font-weight: 600;
     color: ${({ theme }) => theme.color.woodsmoke};
 
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileSmall}px) {
+		font-size:13px;
+        }
+
     ${({ $onMoviePhoto }) => $onMoviePhoto && css`
     font-size:30px;
     line-height: 1.3;
     font-weight: 500;
     color: ${({ theme }) => theme.color.white};
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileLarge}px) {
+		font-size:20px;
+        }
     `}
 
     ${({ $movieDetails }) => $movieDetails && css`
@@ -51,11 +79,16 @@ export const RatingMaxValue = styled.span`
     font-weight: 400;
     line-height: 1.2;
 
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileSmall}px) {
+		font-size:13px;
+        }
+
     ${({ $onMoviePhoto }) => $onMoviePhoto && css`
         display: block;
         margin-top: 10px;
         font-size: 16px;
         color: ${({ theme }) => theme.color.white};
+        
     `}
 
     ${({ $movieDetails }) => $movieDetails && css`
@@ -71,6 +104,10 @@ export const RatingVotesAmount = styled.span`
 	font-size: 16px;
 	font-weight: 400;
 	line-height: 1.5;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileSmall}px) {
+		font-size:13px;
+        }
 
     ${({ $onMoviePhoto }) => $onMoviePhoto && css`
         font-size: 16px;
