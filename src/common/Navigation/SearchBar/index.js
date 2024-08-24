2c3d884c50edd38435794226e6411b2d58696dc2
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-24 19:00:40 +0200

    updated SearchBar placeholder to be dynamic based on the current path

--- src/common/Navigation/SearchBar/index.js
+++ src/common/Navigation/SearchBar/index.js
@@ -1,9 +1,19 @@
 import { Wrapper, SearchInput } from "./styled";
+import { useLocation } from "react-router-dom";
+import {
+	toMovies,
+	toPeople,
+	toMovieDetails,
+	toPeopleDetails,
+} from "../../../core/routes";
 
-export const SearchBar = () => (
-    <Wrapper>
-        <SearchInput
-        placeholder={ "Search for movies..." }
-        />
-    </Wrapper>
-);
+export const SearchBar = () => {
+	const location = useLocation();
+    const isMovies = location.pathname.startsWith("/movies") || location.pathname.startsWith("/movieDetails");
+
+	return (
+		<Wrapper>
+			<SearchInput  placeholder={isMovies ? "Search for movies..." : "Search for people..."} />
+		</Wrapper>
+	);
+};
