3c24d3596f608e57c56e98298e2e73816c852bc3
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-26 17:47:12 +0200

    added peopleListSaga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,8 +1,11 @@
 import { all } from "redux-saga/effects";
 import { movieListSaga } from "../features/movies/MovieList/movieListSaga";
+import { peopleListSaga } from "../features/people/PeopleList/peopleListSaga";
+
 
 export default function* rootSaga() {
     yield all([
         movieListSaga(),
+        peopleListSaga()
     ]);
 };
