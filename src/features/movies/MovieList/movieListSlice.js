b0b24f5b3e81deb0b998c96f0571039d7ddf0aee
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-26 23:31:10 +0200

    Created setError action in movieListSlice

--- src/features/movies/MovieList/movieListSlice.js
+++ src/features/movies/MovieList/movieListSlice.js
@@ -14,16 +14,21 @@ const movieListSlice = createSlice({
             state.status = "success";
             state.movies = movies;
         },
+        setError: (state) => {
+            state.status = "error";
+        },
     },
 });
 
 export const {
     setMovieList,
     setStatus,
+    setError,
 } = movieListSlice.actions;
 
 const selectMovieListState = (state) => state.movieList;
 
+export const selectMovieStatus = (state) => selectMovieListState(state).status;
 export const selectMovieList = (state) => selectMovieListState(state).movies;
 
 export default movieListSlice.reducer;
