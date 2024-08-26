96232e8f76417c54d2b43169cbd6e3cb82e5f943
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-27 00:24:58 +0200

    Added function processMovieListData which returns an object with processed movie data

--- src/API/processAPIData.js
+++ src/API/processAPIData.js
@@ -1,3 +1,5 @@
+import { posterURL } from "./APIdata";
+
 const nameGenres = (genreIds, genres) => {
     return (
         (genreIds || []).map(
@@ -5,5 +7,20 @@ const nameGenres = (genreIds, genres) => {
                 (genre) => genre.id === id).name
         )
     )
-}
-    ;
+};
+
+export const processMovieListData = (movieListData, genreList) => {
+    const genres = genreList.genres;
+
+    return movieListData.map((movie) => (
+        {
+            id: movie.id,
+            title: movie.title,
+            year: movie.release_date,
+            rating: movie.vote_average,
+            vote_count: movie.vote_count,
+            namedGenres: nameGenres(movie.genre_ids, genres),
+            poster: `${posterURL}${movie.poster_path}`
+        }
+    ));
+};
