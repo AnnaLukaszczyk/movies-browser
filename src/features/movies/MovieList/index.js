1fa2c21ea479bf1050ac0a5285497e5eab74ab30
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-26 14:38:14 +0200

    Refactored MovieList

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -4,36 +4,52 @@ import { MovieTileLarge, MovieTileSmall } from "../../../common/Tile";
 import { List, ListItem, StyledLink } from "./styled";
 import { toMovieDetails } from "../../../core/routes";
 import { StyledMain as Main } from "../../../common/Main/styled";
+import { useDispatch, useSelector } from "react-redux";
+import { selectMovieList, setStatus } from "./movieListSlice";
+import { useEffect } from "react";
 
 export const MovieList = () => {
 	const isLargeScreen = useMediaQuery({ query: "(min-width: 993px)" });
+
+	const dispatch = useDispatch();
+	const movies = useSelector(selectMovieList);
+
+	useEffect(() => {
+		dispatch(setStatus())
+	},
+		[]);
 
 	return (
 		<Main>
 			<Section>
 				<SectionTitle>Popular movies</SectionTitle>
 				<List>
-					<StyledLink to={toMovieDetails()}>
-						<ListItem>
-							{isLargeScreen ? (
-								<MovieTileLarge
-									ratingValue="8.1"
-									voteAmount="2000"
-									title="Mulan"
-									year="2020"
-									tag="Action"
-								/>
-							) : (
-								<MovieTileSmall
-									ratingValue="8.1"
-									voteAmount="2000"
-									title="Mulan"
-									year="2020"
-									tag="Action"
-								/>
-							)}
+					{movies.map(({ title, id, vote_average, vote_count, release_date, genre_ids, poster_path }) => (
+						<ListItem key={id}>
+							<StyledLink to={toMovieDetails()}>
+								{isLargeScreen ? (
+									<MovieTileLarge
+										poster={poster_path}
+										ratingValue={vote_average}
+										voteAmount={vote_count}
+										title={title}
+										year={release_date}
+										tags={genre_ids}
+									/>
+								) : (
+									<MovieTileSmall
+										poster={poster_path}
+										ratingValue={vote_average}
+										voteAmount={vote_count}
+										title={title}
+										year={release_date}
+										tags={genre_ids}
+									/>
+								)}
+							</StyledLink>
 						</ListItem>
-					</StyledLink>
+					))}
+
 				</List>
 			</Section>
 		</Main>
