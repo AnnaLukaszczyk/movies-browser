e80c843c8ebb83a3ead8dc02cfa1dc0e6b50c984
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-26 14:51:30 +0200

    Added ImagesURL and posterURL to APIdata

--- src/API/APIdata.js
+++ src/API/APIdata.js
@@ -1,4 +1,8 @@
 export const APIkey = "3f7fc9c72e59bba6e2321914c19c0c85";
 export const AuthorizationAndLanguage = `?api_key=${APIkey}&language=en-US`;
 
+
 export const APIbaseURL = "https://api.themoviedb.org/3/";
+export const ImagesURL = "https://image.tmdb.org/t/p/";
+
+export const posterURL = `${ImagesURL}w300`
