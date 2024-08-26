9d4fd33e23c5ea74f1b12c0404f3c1e2a20cffce
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-26 10:43:02 +0200

    Added Provider component

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./core/theme";
 import { GlobalStyle } from "./core/GlobalStyle";
+import { Provider } from "react-redux";
+import store from "./core/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			<App />
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<App />
+			</ThemeProvider>
+		</Provider>
 	</React.StrictMode>
 );
 
