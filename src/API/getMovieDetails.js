2d55e2c4f0eb67267f93d6e8348b139867765dc1
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-08-27 11:25:36 +0200

    getMovieDetails

--- src/API/getMovieDetails.js
+++ src/API/getMovieDetails.js
@@ -1,20 +1,44 @@
+import axios from "axios";
 import { APIbaseURL } from "./APIdata";
 import { AuthorizationAndLanguage } from "./APIdata";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
-export const getMovieDetails = async () => {
-    try {
-        const response = await fetch(
-            `${APIbaseURL}movie/details${AuthorizationAndLanguage}&page=1`
-        );
+export const useMovieDetails = () => {
+    const movie = useParams();
 
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-        const responseJson = await response.json();
-        const responseResult = await responseJson.results;
-        return responseResult;
+    const url = `${APIbaseURL}movie${AuthorizationAndLanguage}&page=1`;
 
-    } catch (error) {
-        throw error;
-    }
+    const [loading, setLoading] = useState(true);
+
+    const [movieDetails, setMovieDetails] = useState({
+        data: [],
+        status: "loading",
+    });
+
+    useEffect(() => {
+        const getMovieDetails = async () => {
+            try {
+                const response = await axios.get(url);
+                setMovieDetails({
+                    data: response.data,
+                    status: "success",
+                });
+            } catch (error) {
+                setMovieDetails({
+                    status: "error",
+                });
+                console.error(error.message);
+            }
+            finally {
+                setTimeout(() => {
+                    setLoading(false);
+                }, 500);
+            }
+        };
+
+        setTimeout(getMovieDetails, 0);
+    }, [url]);
+
+    return { loading, movieDetails };
 };
