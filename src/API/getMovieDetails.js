493aa10cb3b48f315e1af720a2c24040f8dd3089
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-08-27 12:08:22 +0200

    api link with movie id

--- src/API/getMovieDetails.js
+++ src/API/getMovieDetails.js
@@ -7,7 +7,7 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 export const useMovieDetails = () => {
     const movie = useParams();
 
-    const url = `${APIbaseURL}movie${AuthorizationAndLanguage}&page=1`;
+    const url = `${APIbaseURL}/movie/${movie.id}${AuthorizationAndLanguage}&page=1`;
 
     const [loading, setLoading] = useState(true);
 
