f6d877371bde600f206415b71e560f8267a8d5cf
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-27 13:07:01 +0200

    Added MovieList rendering depending on status

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -5,53 +5,63 @@ import { List, ListItem, StyledLink } from "./styled";
 import { toMovieDetails } from "../../../core/routes";
 import { StyledMain as Main } from "../../../common/Main/styled";
 import { useDispatch, useSelector } from "react-redux";
-import { selectMovieList, setStatus } from "./movieListSlice";
+import { selectMovieList, selectMovieListStatus, setStatus } from "./movieListSlice";
 import { useEffect } from "react";
+import { Loading } from "../../../common/Loading";
+import { Error } from "../../../common/Error";
 
 export const MovieList = () => {
 	const isLargeScreen = useMediaQuery({ query: "(min-width: 993px)" });
 
 	const dispatch = useDispatch();
 	const movies = useSelector(selectMovieList);
+	const status = useSelector(selectMovieListStatus);
 
 	useEffect(() => {
 		dispatch(setStatus())
 	},
 		[]);
 
-	return (
-		<Main>
-			<Section>
-				<SectionTitle>Popular movies</SectionTitle>
-				<List>
-					{movies.map(({ title, id, rating, vote_count, release_date, namedGenres, poster }) => (
-						<ListItem key={id}>
-							<StyledLink to={toMovieDetails()}>
-								{isLargeScreen ? (
-									<MovieTileLarge
-										poster={poster}
-										ratingValue={rating}
-										voteAmount={vote_count}
-										title={title}
-										year={release_date}
-										tags={namedGenres}
-									/>
-								) : (
-									<MovieTileSmall
-										poster={poster}
-										ratingValue={rating}
-										voteAmount={vote_count}
-										title={title}
-										year={release_date}
-										tags={namedGenres}
-									/>
-								)}
-							</StyledLink>
-						</ListItem>
-					))}
+	switch (status) {
+		case "loading":
+			return <Loading />;
+		case "error":
+			return <Error />;
+		default:
+			return (
+				<Main>
+					<Section>
+						<SectionTitle>Popular movies</SectionTitle>
+						<List>
+							{movies.map(({ title, id, rating, vote_count, release_date, namedGenres, poster }) => (
+								<ListItem key={id}>
+									<StyledLink to={toMovieDetails()}>
+										{isLargeScreen ? (
+											<MovieTileLarge
+												poster={poster}
+												ratingValue={rating}
+												voteAmount={vote_count}
+												title={title}
+												year={release_date}
+												tags={namedGenres}
+											/>
+										) : (
+											<MovieTileSmall
+												poster={poster}
+												ratingValue={rating}
+												voteAmount={vote_count}
+												title={title}
+												year={release_date}
+												tags={namedGenres}
+											/>
+										)}
+									</StyledLink>
+								</ListItem>
+							))}
 
-				</List>
-			</Section>
-		</Main>
-	);
-};
+						</List>
+					</Section>
+				</Main>
+			);
+	};
+}
