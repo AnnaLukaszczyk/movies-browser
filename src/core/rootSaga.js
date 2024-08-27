22a4abdeae79b3783547e0357ad0a02d9eea3c4e
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-08-27 17:42:09 +0200

    rootSaga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,11 +1,12 @@
 import { all } from "redux-saga/effects";
 import { movieListSaga } from "../features/movies/MovieList/movieListSaga";
 import { peopleListSaga } from "../features/people/PeopleList/peopleListSaga";
-
+import { movieCreditsSaga } from "../features/movies/MovieDetails/movieCreditsSaga";
 
 export default function* rootSaga() {
     yield all([
         movieListSaga(),
-        peopleListSaga()
+        peopleListSaga(),
+        movieCreditsSaga()
     ]);
 };
