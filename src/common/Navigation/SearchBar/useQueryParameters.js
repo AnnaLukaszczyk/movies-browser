318813c5ef1eedf6bfaccd2bc43e6a478c62fa1b
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-28 17:53:31 +0200

    added `useReplaceQueryParameter` for managing and updating query parameters in URL

--- src/common/Navigation/SearchBar/useQueryParameters.js
+++ src/common/Navigation/SearchBar/useQueryParameters.js
@@ -1,7 +1,41 @@
-import { useLocation } from "react-router-dom/cjs/react-router-dom";
+import { useLocation, useHistory } from "react-router-dom/cjs/react-router-dom";
 
 export const useQueryParameter = (target) => {
-    const location = useLocation();
+	const location = useLocation();
 
-    return (new URLSearchParams(location.search)).get(target);
+	return new URLSearchParams(location.search).get(target);
+};
+
+export const useReplaceQueryParameter = () => {
+	const location = useLocation();
+	const history = useHistory();
+
+	const searchParams = new URLSearchParams(location?.search || "");
+	const isMovies =
+		location.pathname.startsWith("/movies") ||
+		location.pathname.startsWith("/movieDetails");
+
+	return ({ key, value }) => {
+		if (value === undefined) {
+			searchParams.delete(key);
+		} else {
+			searchParams.set(key, value);
+		}
+
+		let newPath = isMovies ? "/movies/search" : "/people/search";
+
+		if (key === "query" && !value) {
+			newPath = isMovies ? "/movies" : "/people";
+		}
+
+		const currentPage = parseInt(searchParams.get("page")) || 1;
+		const currentQuery = searchParams.get("query");
+		let urlParams = `page=${currentPage}`;
+
+		if (currentQuery) {
+			urlParams += `&query=${currentQuery}`;
+		}
+
+		history.replace(`${newPath}?${urlParams}`);
+	};
 };
