2dec53413a19c0ba6388d6deb2364f973620d3bc
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-28 17:47:03 +0200

    added functionality to update "query" parameter on input change

--- src/common/Navigation/SearchBar/index.js
+++ src/common/Navigation/SearchBar/index.js
@@ -1,13 +1,33 @@
 import { Wrapper, SearchInput } from "./styled";
 import { useLocation } from "react-router-dom";
+import {
+	useQueryParameter,
+	useReplaceQueryParameter,
+} from "./useQueryParameters";
 
 export const SearchBar = () => {
 	const location = useLocation();
-    const isMovies = location.pathname.startsWith("/movies") || location.pathname.startsWith("/movieDetails");
+	const isMovies =
+		location.pathname.startsWith("/movies") ||
+		location.pathname.startsWith("/movieDetails");
+
+	const query = useQueryParameter("query");
+	const replaceQueryParameter = useReplaceQueryParameter();
+
+	const onInputChange = ({ target }) => {
+		replaceQueryParameter({
+			key: "query",
+			value: target.value.trim() !== "" ? target.value : undefined,
+		});
+	};
 
 	return (
 		<Wrapper>
-			<SearchInput  placeholder={isMovies ? "Search for movies..." : "Search for people..."} />
+			<SearchInput
+				value={query || ""}
+				onChange={onInputChange}
+				placeholder={isMovies ? "Search for movies..." : "Search for people..."}
+			/>
 		</Wrapper>
 	);
 };
