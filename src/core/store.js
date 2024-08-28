f283c6d85b1dfb3a7fc43c87969d3d1a577f4615
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-08-28 09:56:57 +0200

    store credits

--- src/core/store.js
+++ src/core/store.js
@@ -3,6 +3,7 @@ import createSagaMiddleware from 'redux-saga';
 import rootSaga from "./rootSaga";
 import movieListReducer from "../features/movies/MovieList/movieListSlice";
 import peopleListReducer from '../features/people/PeopleList/peopleListSlice';
+import movieCreditsReducer from '../features/movies/MovieDetails/movieCreditsSlice'; 
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -10,7 +11,7 @@ const store = configureStore({
     reducer: {
         movieList: movieListReducer,
         peopleList: peopleListReducer,
-       
+        movieCredits: movieCreditsReducer,
     },
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(sagaMiddleware),
