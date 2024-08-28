6b69be016a5e35ab43d1b61eac48177f02e19aff
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-28 19:07:35 +0200

    removed "activeClassName" from StyledNavLink, added li

--- src/common/Navigation/NavBar/index.js
+++ src/common/Navigation/NavBar/index.js
@@ -1,29 +1,31 @@
 import { toMovies, toPeople } from "../../../core/routes";
-import { LogoText, NavSection, StyledLink, StyledNavLink, VideoIcon, IconContainer, StyledHeader } from "./styled";
+import {
+	LogoText,
+	NavSection,
+	StyledLink,
+	StyledNavLink,
+	VideoIcon,
+	IconContainer,
+	StyledHeader,
+} from "./styled";
 
 export const NavBar = () => (
-    <StyledHeader>
-        <StyledLink to="/">
-            <IconContainer>
-                <VideoIcon />
-                <LogoText>Movies Browser</LogoText>
-            </IconContainer>
-        </StyledLink>
-        <nav>
-            <NavSection>
-                    <StyledNavLink
-                        activeClassName="active"
-                        to={toMovies()}
-                    >
-                        MOVIES
-                    </StyledNavLink>
-                    <StyledNavLink
-                        activeClassName="active"
-                        to={toPeople()}
-                    >
-                        PEOPLE
-                    </StyledNavLink>
-            </NavSection>
-        </nav>
-    </StyledHeader>
+	<StyledHeader>
+		<StyledLink to="/">
+			<IconContainer>
+				<VideoIcon />
+				<LogoText>Movies Browser</LogoText>
+			</IconContainer>
+		</StyledLink>
+		<nav>
+			<NavSection>
+				<li>
+					<StyledNavLink to={toMovies()}>MOVIES</StyledNavLink>
+				</li>
+				<li>
+					<StyledNavLink to={toPeople()}>PEOPLE</StyledNavLink>
+				</li>
+			</NavSection>
+		</nav>
+	</StyledHeader>
 );
