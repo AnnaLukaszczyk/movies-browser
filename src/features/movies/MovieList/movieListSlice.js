945daa3d5c3602c0922ca5c959ede7ee93429154
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-29 22:59:07 +0200

    Imported paginationReducers from paginationActions to movieListSLice

--- src/features/movies/MovieList/movieListSlice.js
+++ src/features/movies/MovieList/movieListSlice.js
@@ -1,13 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { paginationActions } from '../../../common/Pagination/createPaginationActions';
+
+const { paginationReducers } = paginationActions("movieListSLice");
 
 const movieListSlice = createSlice({
     name: "movieList",
     initialState: {
+        page: undefined,
         movies: [],
         genres: [],
         status: "initial",
     },
     reducers: {
+        ...paginationReducers,
         setStatus: (state) => {
             state.status = "loading";
         },
@@ -26,6 +31,7 @@ const movieListSlice = createSlice({
 });
 
 export const {
+    pageNumberFromURL,
     setMovieList,
     setStatus,
     setGenres,
@@ -35,6 +41,7 @@ export const {
 const selectMovieListState = (state) => state.movieList;
 
 export const selectMovieListStatus = (state) => selectMovieListState(state).status;
+export const selectMovieListPage = (state) => selectMovieListState(state).page;
 export const selectMovieList = (state) => selectMovieListState(state).movies;
 
 export const selectGenres = (state) => selectMovieListState(state).genres;
