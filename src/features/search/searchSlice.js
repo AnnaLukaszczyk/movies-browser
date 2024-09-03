cdc6e12a495eed48bfbe631e04567018f8454e75
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-09-04 00:13:32 +0200

    Added setData, setTotalResult, setTotalPages to searchSlice

--- src/features/search/searchSlice.js
+++ src/features/search/searchSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 import { paginationActions } from "../../common/Pagination/createPaginationActions";
 
-const { paginationReducers } = paginationActions('searchSlice');
+const { paginationReducer } = paginationActions('searchSlice');
 
 const searchSlice = createSlice({
     name: "search",
@@ -10,16 +10,28 @@ const searchSlice = createSlice({
         path: "",
         inputValue: "",
         page: 1,
-       
+        data: [],
+        totalResults: 0,
+        totalPages: 0,
     },
     reducers: {
-        ...paginationReducers,
+        ...paginationReducer,
         setPath: (state, { payload: path }) => {
             state.path = path;
         },
         setInputValue: (state, { payload: query }) => {
             state.inputValue = query;
         },
+        setData: (state, {payload: rawResult}) => {
+            state.data = rawResult.results;
+        },
+        setTotalResult: (state, {payload: rawResult}) => {
+            state.totalResults = rawResult.total_results;
+        },
+        setTotalPages: (state, {payload: rawResult}) => {
+            state.totalPages = rawResult.total_pages;
+        }
+
     }
 })
 
@@ -27,11 +39,17 @@ export const {
     pageNumberFromURL,
     setPath,
     setInputValue,
+    setData,
+    setTotalResult,
+    setTotalPages,
 } = searchSlice.actions;
 
 const selectSearchState = (state) => state.search;
 export const selectPath = (state) => selectSearchState(state).path;
 export const selectInputValue = (state) => selectSearchState(state).inputValue;
 export const selectPage = (state) => selectSearchState(state).page;
+export const selectSearchData = (state) => selectSearchState(state).data;
+export const selectSearchTotalResults = (state) => selectSearchState(state).totalResults;
+export const selectTotalPages = (state) => selectSearchState(state).totalPages;
 
 export default searchSlice.reducer;
