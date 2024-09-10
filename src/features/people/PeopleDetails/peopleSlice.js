eb8892b6eea87d7dc5c6ed100efaf34207a13ad3
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-09-10 21:30:08 +0200

    extended peopleSlice to handle cast and crew movies

--- src/features/people/PeopleDetails/peopleSlice.js
+++ src/features/people/PeopleDetails/peopleSlice.js
@@ -5,6 +5,8 @@ const peopleSlice = createSlice({
 	initialState: {
 		id: "",
 		details: {},
+		castMovies: [],
+		crewMovies: [],
 	},
 	reducers: {
 		setPeopleId: (state, { payload: personId }) => {
@@ -13,13 +15,28 @@ const peopleSlice = createSlice({
 		setPeopleDetails: (state, { payload: details }) => {
 			state.details = details;
 		},
+		setPeopleCastMovies: (state, { payload: castMovies }) => {
+			state.castMovies = castMovies;
+		},
+		setPeopleCrewMovies: (state, { payload: crewMovies }) => {
+			state.crewMovies = crewMovies;
+		},
 	},
 });
 
-export const { setPeopleId, setPeopleDetails } = peopleSlice.actions;
+export const {
+	setPeopleId,
+	setPeopleDetails,
+	setPeopleCastMovies,
+	setPeopleCrewMovies,
+} = peopleSlice.actions;
 
 export const selectPeopleState = (state) => state.people;
 export const selectPeopleId = (state) => selectPeopleState(state).id;
 export const selectPeopleDetails = (state) => selectPeopleState(state).details;
+export const selectPeopleCastMovies = (state) =>
+	selectPeopleState(state).castMovies;
+export const selectPeopleCrewMovies = (state) =>
+	selectPeopleState(state).crewMovies;
 
 export default peopleSlice.reducer;
