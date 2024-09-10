ae4ba7c95c32ce69e40b0644c8c15fa262856a07
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-09-10 21:29:20 +0200

    enhanced peopleSaga to fetch and set cast and crew movies

--- src/features/people/PeopleDetails/peopleSaga.js
+++ src/features/people/PeopleDetails/peopleSaga.js
@@ -1,16 +1,24 @@
 import { call, put, takeEvery, select } from "redux-saga/effects";
 import {
 	setPeopleDetails,
+	setPeopleCastMovies,
+	setPeopleCrewMovies,
 	selectPeopleId,
 	setPeopleId,
 } from "./peopleSlice";
 import { getPeopleDetails } from "../../../API/getPeopleDetails";
+import { getPeopleCastMovies } from "../../../API/getPeopleCastMovies";
+import { getPeopleCrewMovies } from "../../../API/getPeopleCrewMovies";
 
 function* fetchPeopleHandler() {
 	try {
 		const personId = yield select(selectPeopleId);
 		const details = yield call(getPeopleDetails, personId);
+		const castMovies = yield call(getPeopleCastMovies, personId);
+		const crewMovies = yield call(getPeopleCrewMovies, personId);
 		yield put(setPeopleDetails(details));
+		yield put(setPeopleCastMovies(castMovies));
+		yield put(setPeopleCrewMovies(crewMovies));
 	} catch (error) {
 		console.error("Error while fetching peopleDetails", error);
 	}
