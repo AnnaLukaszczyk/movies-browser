a48d8861b6303ede92ad96e6d3a71187aca4c5e9
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-09-11 01:57:17 +0200

    Used SearchResults component in SearchMovieList and SearchPeopleList

--- src/common/SearchResults/index.js
+++ src/common/SearchResults/index.js
@@ -2,14 +2,10 @@ import { useLocation } from "react-router-dom";
 import { Section, SectionTitle } from "../Section";
 import queryParamName from "../../queryParamName";
 
-export const SearchResults = () => {
-	const location = useLocation();
-	const searchParams = new URLSearchParams(location.search);
-	const query = searchParams.get(queryParamName) || "";
-
+export const SearchResults = ({searchQuery, searchTotalResults}) => {
 	return (
 		<Section>
-			<SectionTitle>Search results for "{query}"</SectionTitle>
+			<SectionTitle>Search results for {`"${searchQuery}"(${searchTotalResults})`}</SectionTitle>
 		</Section>
 	);
 };
