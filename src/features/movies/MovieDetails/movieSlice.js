f16c22ba45e926f65913474388384b7b75ca6e22
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-09-11 15:30:45 +0200

    added loading status and error handling to movieSlice

--- src/features/movies/MovieDetails/movieSlice.js
+++ src/features/movies/MovieDetails/movieSlice.js
@@ -1,30 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const movieSlice = createSlice({
-    name: "movie",
-    initialState: {
-        id: "",
-        details: [],
-        credits: [],
-    },
-    reducers: {
-        setMovieId: (state, { payload: movieId }) => {
-            state.id = movieId;
-        },
-        setMovie: (state, { payload: details }) => {
-            state.details = details;
-        },
-        setCredits: (state, { payload: credits }) => {
-            state.credits = credits;
-        },
-    },
+	name: "movie",
+	initialState: {
+		id: "",
+		details: [],
+		credits: [],
+		status: "initial",
+	},
+	reducers: {
+		setMovieId: (state, { payload: movieId }) => {
+			state.id = movieId;
+			state.status = "loading";
+		},
+		setMovie: (state, { payload: details }) => {
+			state.details = details;
+			state.status = "success";
+		},
+		setCredits: (state, { payload: credits }) => {
+			state.credits = credits;
+			state.status = "success";
+		},
+		setError: (state) => {
+			state.status = "error";
+		},
+	},
 });
 
-export const {
-    setMovieId,
-    setMovie,
-    setCredits,
-} = movieSlice.actions;
+export const { setMovieId, setMovie, setCredits, setError } =
+	movieSlice.actions;
 
 export const selectMovieState = (state) => state.movie;
 export const selectMovieId = (state) => selectMovieState(state).id;
@@ -33,5 +37,6 @@ export const selectMovieCredits = (state) => selectMovieState(state).credits;
 export const selectMovieCast = (state) => selectMovieCredits(state).cast;
 export const selectMovieCrew = (state) => selectMovieCredits(state).crew;
 export const selectMoviId = (state) => selectMovieCredits(state).id;
+export const selectMovieStatus = (state) => selectMovieState(state).status;
 
 export default movieSlice.reducer;
