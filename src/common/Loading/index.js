1d41d8ce518410d32b07e8e5b530b4061cff9341
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-09-11 19:20:03 +0200

    removed `Section` component from `Loading` component

--- src/common/Loading/index.js
+++ src/common/Loading/index.js
@@ -1,10 +1,7 @@
-import { Section } from "../Section";
 import { BoxForSpinner, Spinner } from "./styled";
 
 export const Loading = () => (
-	<Section>
 		<BoxForSpinner>
 			<Spinner />
 		</BoxForSpinner>
-	</Section>
 );
