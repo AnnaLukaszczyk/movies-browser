9ca3d9da0df111f3a548c32de4ddbe9ec4a555d2
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-09-11 15:26:54 +0200

    added support for `loading` and `error` states in `MovieDetails` component

--- src/features/movies/MovieDetails/index.js
+++ src/features/movies/MovieDetails/index.js
@@ -8,12 +8,18 @@ import {
 	Section,
 	List,
 } from "./styled";
+import { Loading } from "../../../common/Loading";
+import { Error } from "../../../common/Error";
 import { SectionTitle } from "../../../common/Section";
 import { MovieDetailsTile, PeopleTile } from "../../../common/Tile";
 import { StyledMain as Main } from "../../../common/Main/styled";
 import { ListItem, StyledLink } from "../../people/PeopleList/styled";
 import { toPeopleDetails } from "../../../core/routes";
-import { useHistory, useLocation, useParams } from "react-router-dom/cjs/react-router-dom";
+import {
+	useHistory,
+	useLocation,
+	useParams,
+} from "react-router-dom/cjs/react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import {
@@ -21,12 +27,12 @@ import {
 	selectMovieCast,
 	selectMovieCrew,
 	setMovieId,
+	selectMovieStatus,
 } from "./movieSlice";
 import queryParamName from "../../../queryParamName";
 import { SearchPage } from "../../search";
 import { useQueryParameter } from "../../../common/Navigation/SearchBar/useQueryParameters";
 import pageParamName from "../../../paginationParam";
-
 
 export const MovieDetails = () => {
 	const params = useParams();
@@ -37,7 +43,6 @@ export const MovieDetails = () => {
 	const searchParams = new URLSearchParams(location.search);
 
 	const query = useQueryParameter(queryParamName);
-
 
 	useEffect(() => {
 		dispatch(setMovieId(params.id));
@@ -51,78 +56,87 @@ export const MovieDetails = () => {
 	const movie = useSelector(selectMovie);
 	const cast = useSelector(selectMovieCast);
 	const crew = useSelector(selectMovieCrew);
+	const status = useSelector(selectMovieStatus);
 
 	if (query) {
-		return <SearchPage />
+		return <SearchPage />;
 	}
 
-	return (
-		<>
-			<Header>
-				<BackgroundImage poster={movie.backdrop}>
-					<Vignette />
-					<TitleContainer>
-						<Title>{movie.title}</Title>
-						<Rating
-							isOnMoviePhoto
+	switch (status) {
+		case "loading":
+			return <Loading />;
+		case "error":
+			return <Error />;
+		default:
+			return (
+				<>
+					<Header>
+						<BackgroundImage poster={movie.backdrop}>
+							<Vignette />
+							<TitleContainer>
+								<Title>{movie.title}</Title>
+								<Rating
+									isOnMoviePhoto
+									ratingValue={movie.rating}
+									voteAmount={movie.voteCount}
+								/>
+							</TitleContainer>
+						</BackgroundImage>
+					</Header>
+					<Main>
+						<MovieDetailsTile
+							poster={movie.poster}
 							ratingValue={movie.rating}
 							voteAmount={movie.voteCount}
+							title={movie.title}
+							year={movie.releaseYear}
+							production={movie.production}
+							date={
+								movie.releaseDate
+									? new Date(movie.releaseDate).toLocaleDateString()
+									: "Unknown"
+							}
+							tags={movie.genres}
+							description={movie.description}
 						/>
-					</TitleContainer>
-				</BackgroundImage>
-			</Header>
-			<Main>
-				<MovieDetailsTile
-					poster={movie.poster}
-					ratingValue={movie.rating}
-					voteAmount={movie.voteCount}
-					title={movie.title}
-					year={movie.releaseYear}
-					production={movie.production}
-					date={movie.releaseDate
-						? (new Date(movie.releaseDate).toLocaleDateString())
-						: "Unknown"
-					}
-					tags={movie.genres}
-					description={movie.description}
-				/>
-				<Section>
-					<SectionTitle>Cast</SectionTitle>
-					{cast && (
-						<List>
-							{cast.map(({ id, name, character, profile_path }) => (
-								<StyledLink to={toPeopleDetails({ id: id })}>
-									<ListItem key={id}>
-										<PeopleTile
-											profilePath={profile_path}
-											name={name}
-											character={character}
-										/>
-									</ListItem>
-								</StyledLink>
-							))}
-						</List>
-					)}
-				</Section>
-				<Section>
-					<SectionTitle>Crew</SectionTitle>
-					{crew && (
-						<List>
-							{crew.map(({ id, name, profile_path, job }) => (
-								<StyledLink to={toPeopleDetails({ id: id })}>
-									<ListItem key={id}>
-										<PeopleTile
-											profilePath={profile_path}
-											name={name}
-											role={job}
-										/>
-									</ListItem>
-								</StyledLink>
-							))}
-						</List>
-					)}
-				</Section>
-			</Main>
-		</>
-	);
+						<Section>
+							<SectionTitle>Cast</SectionTitle>
+							{cast && (
+								<List>
+									{cast.map(({ id, name, character, profile_path }) => (
+										<StyledLink to={toPeopleDetails({ id: id })}>
+											<ListItem key={id}>
+												<PeopleTile
+													profilePath={profile_path}
+													name={name}
+													character={character}
+												/>
+											</ListItem>
+										</StyledLink>
+									))}
+								</List>
+							)}
+						</Section>
+						<Section>
+							<SectionTitle>Crew</SectionTitle>
+							{crew && (
+								<List>
+									{crew.map(({ id, name, profile_path, job }) => (
+										<StyledLink to={toPeopleDetails({ id: id })}>
+											<ListItem key={id}>
+												<PeopleTile
+													profilePath={profile_path}
+													name={name}
+													role={job}
+												/>
+											</ListItem>
+										</StyledLink>
+									))}
+								</List>
+							)}
+						</Section>
+					</Main>
+				</>
+			);
+	}
 };
