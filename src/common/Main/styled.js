8cc477d0e9e7558310126725827f0e9f78ff3621
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-09-13 13:27:35 +0200

    added padding-bottom to StyledMain

--- src/common/Main/styled.js
+++ src/common/Main/styled.js
@@ -3,13 +3,15 @@ import styled from "styled-components";
 export const StyledMain = styled.main`
 	max-width: 1368px;
 	margin: 56px auto;
+    padding-bottom: 103px;
 
 	@media(max-width: ${({ theme }) => theme.breakpoints.desktopLarge}px) { 
         margin: 56px 4%;
     }
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobileLarge}px) {
-        margin: 24px 5%
+        margin: 24px 5%;
+        padding-bottom: 59px;
 	}
 
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileSmall}px) { 
