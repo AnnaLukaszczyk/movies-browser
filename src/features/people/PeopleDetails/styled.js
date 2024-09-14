d31bf802402fe8b676991003753480c3cd7d1db2
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-09-14 10:51:56 +0200

    improved margin for Section on smallest resolutions

--- src/features/people/PeopleDetails/styled.js
+++ src/features/people/PeopleDetails/styled.js
@@ -3,4 +3,8 @@ import { Section as OriginalSection } from "../../../common/Section";
 
 export const Section = styled(OriginalSection)`
 	margin-top: 64px;
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobileSmall}px) {
+		margin-top: 24px;
+	}
 `;
